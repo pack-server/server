cd5ef9c1e58bfec9b7bb971e1b33b6e4f520dfff
Rememberautumn <4013465w@sina.com>
2016-09-15 10:14:37 +0800

    修复dev scripts

--- controller/index.js
+++ controller/index.js
@@ -2,13 +2,18 @@ const Site = require('../model/site.js');
 const site = new Site();
 module.exports = {
   index: function*() {
-    yield this.render('index', { "title": "koa demo" });
+    yield this.render('index', { "mod": 0 });
   },
   build: function*() {
 
   },
   site: function*(id) {
-    yield this.render('index', { "mod": JSON.stringify(this.params) });
+    let mod = JSON.stringify(this.params);
+    if (!mod) {
+      mod = 0;
+    }
+    console.log(this.params)
+    yield this.render('index', { "mod": mod });
     console.log(this.params);
   }
 }
